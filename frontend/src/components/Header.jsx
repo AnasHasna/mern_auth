501acac8c402bb23bfd93f997a82c614f1f87d40
Anas Hasna <hasnaanas230@gmail.com>
2023-07-16 18:57:57 +0100

    front end set up and header component commit

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,25 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaSignOutAlt, FaSignInAlt } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Container>
+        <Navbar.Brand href="/">MERN Auth</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="/login">
+              <FaSignInAlt /> Sign In
+            </Nav.Link>
+            <Nav.Link href="/register">
+              <FaSignOutAlt /> Sign Up
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
